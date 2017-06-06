91baf39705a827ee470ea5587e49e43dfd1c405a
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-06 21:03:51 +0300

    Create relationships

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug', 'permissions',
+    ];
+    protected $casts = [
+        'permissions' => 'array',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'role_users');
+    }
+
+    /**
+     * @param array $permissions
+     * @return bool
+     */
+    public function hasAccess(array $permissions) // bool
+    {
+        foreach ($permissions as $permission) {
+            if ($this->hasPermission($permission))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $permission
+     * @return bool
+     */
+    private function hasPermission($permission)// bool
+    {
+        return $this->permissions[$permission] ?? false;
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,35 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_users');
+    }
+
+    /**
+     * Checks if User has access to $permissions.
+     * @param array $permissions
+     * @return bool
+     */
+    public function hasAccess(array $permissions)
+    {
+        // check if the permission is available in any role
+        foreach ($this->roles as $role) {
+            if ($role->hasAccess($permissions)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the user belongs to role.
+     * @param $roleSlug
+     * @return bool
+     */
+    public function inRole($roleSlug)
+    {
+        return $this->roles()->where('slug', $roleSlug)->count() == 1;
+    }
 }
