12444f7f275b878c82c0ace1047369295c26ad1f
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-06 21:33:11 +0300

    Posts CRUD

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    $title = $faker->sentence;
+    return [
+        'title' => $title,
+        'slug' => str_slug($title, '-'),
+        'body' => $faker->paragraph,
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+    ];
+});
